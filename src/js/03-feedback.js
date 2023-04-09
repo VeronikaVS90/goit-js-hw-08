fee42b287ce33f819d053b45cfe4df78af799ca0
VeronikaVS90 <vsokolovanika@gmail.com>
2023-04-09 22:35:41 +0300

    Виконала завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const { email, message } = feedbackForm;
+fillForm();
+
+feedbackForm.addEventListener('input', throttle(saveFormData, 500));
+feedbackForm.addEventListener('submit', handleSubmit);
+
+function saveFormData() {
+    
+        const formData = { email: email.value, message: message.value };
+        localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    }
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const formData = { email: email.value, message: message.value };
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    e.currentTarget.reset();
+}
+    
+function fillForm() {
+    const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (formData) {
+        email.value = formData.email;
+        message.value = formData.message;
+    }
+}
 
